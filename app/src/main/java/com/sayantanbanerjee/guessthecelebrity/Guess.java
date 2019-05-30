12ee271711ea0d8162659f0a21b21e8772fcf155
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2019-05-30 11:33:00 +0530

    Download content from web (HTML document) done. Working okay . Taking Huge time to Open and download

--- app/src/main/java/com/sayantanbanerjee/guessthecelebrity/Guess.java
+++ app/src/main/java/com/sayantanbanerjee/guessthecelebrity/Guess.java
@@ -1,9 +1,16 @@
 package com.sayantanbanerjee.guessthecelebrity;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Guess extends AppCompatActivity {
 
@@ -15,10 +22,52 @@ public class Guess extends AppCompatActivity {
 
     }
 
+    public class DownloadTask extends AsyncTask<String , Void , String>{
+
+        @Override
+        protected String doInBackground(String... string) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try{
+                url  = new URL(string[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while(data != -1)
+                {
+                    char current = (char) data;
+                    result+=current;
+                    data = reader.read();
+                }
+                return  result;
+            }catch(Exception e){
+                e.printStackTrace();
+                return "FAILED";
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guess);
         getSupportActionBar().hide();
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
+
+        try {
+            result = task.execute("https://www.imdb.com/list/ls022546765/").get();
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+        Log.i("Result : ", result);
+
     }
 }
